bc9c160a21cd3fd9e343e3409df068b9654ffcbd
LuisUniverseCity <68173982+LuisUniverseCity@users.noreply.github.com>
2025-12-10 15:09:19 -0600

    Actualizar app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import { React, Dotenv } from './config/env.js';
 Dotenv()
 
@@ -8,13 +7,6 @@ const port = 2828
 async function server() {
     const app = express()
     app.use(express.json())
-    app.use(cors());
-    /*
-    app.use(cors({
-      origin: 'http://example.com', // allow only this origin
-      methods: ['GET', 'POST'],     // allow only these methods
-    }));
-    */
 
     // BACKEND CODE HERE
     // ----------------------------------------------------------------------------------------
