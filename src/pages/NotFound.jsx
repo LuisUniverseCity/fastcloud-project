19976edb5ca58494ec2ad5baafc34a5319a474d4
LuisUniverseCity <68173982+LuisUniverseCity@users.noreply.github.com>
2025-12-21 15:22:20 -0600

    not found page favicon change

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,9 +2,9 @@
 export default function (){
     return(
         <div className="w-200 absolute left-1/2 -translate-x-1/2 top-40 items-center flex flex-col">
-            <img className="mb-5 w-38 h-38" src="/favicon-fc.svg" alt="favicon"/>
-            <p className="text-7xl text-center text-[#134878] mb-5">404</p>
-            <p className="text-5xl text-center text-[#124979]">Page Not Found</p>
+            <img className="mb-5 w-42 h-42" src="/faviconFc.png" alt="favicon"/>
+            <p className="text-7xl text-center text-[#1c9bb8] mb-5">404</p>
+            <p className="text-5xl text-center text-[#1c9bb8]">Page Not Found</p>
         </div>
     )
 }
